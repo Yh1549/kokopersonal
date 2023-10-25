2754ab273c7cd0abfc6b367653298a8541f50c57
鄭翊宏 <>
2023-10-25 11:16:55 +0800

    init

--- src/view/UserEdit.vue
+++ src/view/UserEdit.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>編輯</h1>
+  <div class="w-full flex flex-col justify-center">
+    <div class="self-center flex gap-4 m-4">
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">姓氏</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+        />
+      </div>
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">名字</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+        />
+      </div>
+    </div>
+    <div class="self-center flex gap-4 m-4">
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">電話</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+        />
+      </div>
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">email</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+        />
+      </div>
+    </div>
+  </div>
 </template>
